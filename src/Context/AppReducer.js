5df535a38555db4f4c4dd72ee1e306ec75397969
HARSH JAISWAL <92844041+harshjaiswalll@users.noreply.github.com>
2023-06-07 18:57:36 +0530

    yes

---
+++ src/Context/AppReducer.js
@@ -0,0 +1,25 @@
+export default (state, action) => {
+    switch(action.type) {
+        case 'ADD_CONTACT':
+            return ({
+                ...state,
+                contacts: [ ...state.contacts, action.payload ]
+            }
+            )
+        case 'DELETE_CONTACT':
+            return ({
+                ...state,
+                contacts: state.contacts.filter( (contact) => contact.id!==action.payload)
+            }) 
+        case 'EDIT_CONTACT':
+            return ({
+                ...state,
+                toBeUpdated: state.contacts.filter((contact) => contact.id===action.payload),
+                // contacts: state.contacts.filter( (contact) => contact.id!==action.payload)
+            }) 
+            
+        default:
+            console.log('here too')
+            return state;
+    }
+}
